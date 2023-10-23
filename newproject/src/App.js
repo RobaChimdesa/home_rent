c0a15f46b9eae92c5c107822e89d79f37056fec9
RobaChimdesa <robachimdesa123@gmail.com>
2023-10-23 11:26:10 +0300

    first commit

---
+++ newproject/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./portfolio/navbar";
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div >
+    <Navbar/>
+    </div>
+  );
+}
+
+export default App;
